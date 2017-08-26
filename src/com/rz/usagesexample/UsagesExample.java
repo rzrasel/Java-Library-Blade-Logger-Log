96ed4593c3b3b6665122f790447f5c9c8671c4f1
Developer <developer@Shurjo.local>
2017-08-26 13:20:22 +0600

    Add Project

---
+++ src/com/rz/usagesexample/UsagesExample.java
@@ -0,0 +1,19 @@
+package com.rz.usagesexample;
+
+import com.rz.logger.LogWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author developer
+ */
+public class UsagesExample {
+
+    public static void main(String[] args) {
+        LogWriter.Log("Hi");
+    }
+}
